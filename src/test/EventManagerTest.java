fd81195a61eaa08e6ee7b62764b383ca6e8ab480
seanRong <seanRong@users.noreply.github.com>
2019-10-17 10:05:17 -0700

    ref fix cov

--- src/test/EventManagerTest.java
+++ src/test/EventManagerTest.java
@@ -1,19 +1,17 @@
 import model.Event;
 
+import model.EventManager;
 import model.OneTimeEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ui.AccessibleDashboard;
-import ui.Dashboard;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DashboardTest {
-    private Dashboard dashboard;
-    private AccessibleDashboard accdashboard;
+public class EventManagerTest {
+    private EventManager eventManagerTest;
     private ArrayList<Event> testEventList;
     private OneTimeEvent event;
     private OneTimeEvent anotherEvent;
@@ -21,7 +19,7 @@ public class DashboardTest {
 
     @BeforeEach
     void runBefore() {
-        accdashboard = new AccessibleDashboard();
+        eventManagerTest = new EventManager();
         testEventList = new ArrayList<Event>();
         event = new OneTimeEvent("test event", 20, 2000);
         anotherEvent = new OneTimeEvent("second event", 25, 4500);
@@ -34,14 +32,23 @@ public class DashboardTest {
 
     @Test
     void testAddEvent() {
-        testEventList.add(event);
-        assertTrue(testEventList.contains(event));
+        eventManagerTest.addEvent(event);
+        assertTrue(eventManagerTest.getEventList().contains(event));
     }
 
     @Test
     void testDumpSchedule() {
         testEventList.add(event);
-        assertEquals("test event on 20 at 2000", accdashboard.dumpSchedule(testEventList));
+        assertEquals("test event on 20 at 2000", eventManagerTest.dumpSchedule(testEventList));
     }
+
+    @Test
+    void testSave() {
+        eventManagerTest.addEvent(event);
+        eventManagerTest.save();
+        eventManagerTest.load();
+        assertTrue(eventManagerTest.getEventList().contains(event));
+    }
+
 
 }
