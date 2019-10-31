b958d32be24a94bc7d203e5bf5dfaad4f1efc57c
seanRong <seanRong@users.noreply.github.com>
2019-10-31 12:55:35 -0700

    incr cov

---
+++ src/test/ClassScheduleTest.java
@@ -0,0 +1,31 @@
+import model.*;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClassScheduleTest {
+    ClassSchedule cs = new ClassSchedule();
+    Date defaultDate = new SimpleDateFormat("dd/MM/yyyy").parse("06/20/2099");
+    ClassEvent ce = new ClassEvent("CPSC", defaultDate, 1234);
+
+    public ClassScheduleTest() throws ParseException {
+    }
+
+    @Test
+    void testAdd() {
+        cs.addClass(ce);
+        assertEquals(ce, cs.getEvent(0));
+
+    }
+
+    @Test
+    void testRemove() {
+        cs.addClass(ce);
+        cs.removeClass(ce);
+        assertEquals(0, cs.getSize());
+    }
+}
