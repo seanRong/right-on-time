488a26c0380eee270b20f76141a8579309eacad3
seanRong <seanRong@users.noreply.github.com>
2019-11-25 22:34:21 -0800

    some work done to get it launching

--- src/main/ui/Controller.java
+++ src/main/ui/Controller.java
@@ -1,4 +1,23 @@
 package ui;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void receiveInput(ActionEvent actionEvent) {
+        System.out.println("pressed");
+    }
+
+    public void quit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void load(ActionEvent actionEvent) {
+    }
+
+    public void save(ActionEvent actionEvent) {
+    }
+
+    public void makeEventOneTime(ActionEvent actionEvent) {
+
+    }
 }
